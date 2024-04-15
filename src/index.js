180a36fa415bfd09bb93915b8192c12952c51088
Stacy <stacykimilu@gmail.com>
2024-04-15 14:47:22 +0300

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+      const movieId = event.target.elements['searchByID'].value;
+  
+      fetch(`http://localhost:3000/movies/${movieId}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch movie");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const titleElement = document.querySelector("#movieDetails h4");
+          const summaryElement = document.querySelector("#movieDetails p");
+  
+          titleElement.innerText = data.title;
+          summaryElement.innerText = data.summary;
+        })
+        .catch((error) => {
+          console.error("An error fetching movie :", error);
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
+  
